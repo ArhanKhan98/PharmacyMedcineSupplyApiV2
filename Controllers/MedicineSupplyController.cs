470bdfa401099319dca73401b143e48004009430
ArhanKhan98 <arhansam786@gmail.com>
2020-11-07 18:47:34 +0530

    Second Commit

--- Controllers/MedicineSupplyController.cs
+++ Controllers/MedicineSupplyController.cs
@@ -31,31 +31,37 @@ namespace PharmacyMedicineSupplyApi.Controllers
         [Route("GetSupplies/{medicineName}/{count}")]
         public async Task<IActionResult> GetSupplies(string medicineName,int count)
         {
-            _log4net.Info(" Http GetSupplies request Initiated");
+            _log4net.Info(" Http GetSupplies request Initiated ");
             if (medicineName==""||count==0)
             {
-                _log4net.Error("Null values passed to GetSupplies Method");
+                _log4net.Error("Http GetSupplies Null values passed to GetSupplies Method");
                 return BadRequest("Please provide some values");
             }
             try
             {
+                _log4net.Info("Http GetSupplies request for"+medicineName +" with a demand count of "+count);
+
                 List<PharmacyMedicineSupply> supplylist = await _medicineRepo.MedcineSupply(medicineName, count);
                 if (supplylist != null)
                 {
+                    _log4net.Info(" Http GetSupplies response returned");
                     List<PharmacyMedicineSupplyDto> medincinesupply = new List<PharmacyMedicineSupplyDto>();
                     foreach(var supply in supplylist)
                     {
                         PharmacyMedicineSupplyDto medSupply = _mapper.Map<PharmacyMedicineSupplyDto>(supply);
                         medincinesupply.Add(medSupply);
+
+                        _log4net.Info(" Http GetSupplies Distribution : "+medSupply.PharmacyName+" Has to be supplied "+medSupply.SupplyCount+" of "+medSupply.MedicineName);
                     }
                     return Ok(medincinesupply);
                 }
+                _log4net.Error("Http GetSupplies No values returned");
                 return BadRequest("Some Error While fetching request");
             }
             catch (Exception e)
             {
                 _log4net.Error(" Http GetSupplies encountered an Excpetion :"+e.Message);
-                return BadRequest("Some Exception encountered while processing the request");
+                return NotFound(e.Message);
             }
         }
     }
--- Respository/SupplyRepository.cs
+++ Respository/SupplyRepository.cs
@@ -10,124 +10,82 @@ namespace PharmacyMedicineSupplyApi.Respository
 {
     public class SupplyRepository : ISupply
     {
-        /*   public List<PharmacyMedicineSupply> GetSupplies(List<MedicineDemand> demand)
-           {
-               //List of Supply to be sent
-               List<PharmacyMedicineSupply> supplies = new List<PharmacyMedicineSupply>();
-
-               //List of All the meds in stock
-               var stock = new List<MedicineStock>() { 
-               new MedicineStock{Name="A",ChemicalComposition="a,b,c",DateOfExpiry="02-11-2022",NumberOfTabletsInStock=200,TargetAilment="General"},
-               new MedicineStock{Name="B",ChemicalComposition="a,b,c",DateOfExpiry="02-11-2022",NumberOfTabletsInStock=100,TargetAilment="General"},
-               new MedicineStock{Name="C",ChemicalComposition="a,b,c",DateOfExpiry="02-11-2022",NumberOfTabletsInStock=200,TargetAilment="General"},
-               new MedicineStock{Name="D",ChemicalComposition="a,b,c",DateOfExpiry="02-11-2022",NumberOfTabletsInStock=200,TargetAilment="General"},
-               new MedicineStock{Name="E",ChemicalComposition="a,b,c",DateOfExpiry="02-11-2022",NumberOfTabletsInStock=400,TargetAilment="General"},
-               };
-
-               //Dictionary to store the Name of med and stock value as key value pair
-               Dictionary<string, int> meds = new Dictionary<string, int>();
-               foreach(var med in stock)
-               {
-                   meds.Add(med.Name, med.NumberOfTabletsInStock);
-               }
-
-               //List of Pharmacy the company does business with
-               List<string> Pharmacies = new List<string>() {"Pharmacy1","Pharmacy2","Pharmacy3","Pharmacy4"};
-               int totalPharmacies = Pharmacies.Count;
-               PharmacyMedicineSupply medSupply;
-               foreach(var med in demand)
-               {
-                   int inStock = meds[med.Medicine];
-                   int demandStock = med.Demand;
-                   if(inStock>med.Demand)
-                   {
-                       int medcount = demandStock / totalPharmacies;
-                       for(int i=0;i<totalPharmacies-1;i++)
-                       {
-                           medSupply = new PharmacyMedicineSupply() { MedicineName = med.Medicine, PharmacyName = Pharmacies[i], SupplyCount = medcount };
-                           supplies.Add(medSupply);
-                           demandStock = demandStock - medcount;
-                       }
-                       medSupply = new PharmacyMedicineSupply() { MedicineName = med.Medicine, PharmacyName = Pharmacies[totalPharmacies-1], SupplyCount = demandStock };
-                       supplies.Add(medSupply);
-                   }
-                   else
-                   {
-                       int medcount = inStock / totalPharmacies;
-                       for (int i = 0; i < totalPharmacies - 1; i++)
-                       {
-                           medSupply = new PharmacyMedicineSupply() { MedicineName = med.Medicine, PharmacyName = Pharmacies[i], SupplyCount = medcount };
-                           supplies.Add(medSupply);
-                           inStock = inStock - medcount;
-                       }
-                       medSupply = new PharmacyMedicineSupply() { MedicineName = med.Medicine, PharmacyName = Pharmacies[totalPharmacies - 1], SupplyCount = inStock };
-                       supplies.Add(medSupply);
-                   }
-
-               }
-
-
-
-               return supplies;
-           }*/
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(SupplyRepository));
 
         public async Task<List<PharmacyMedicineSupply>> GetSupplies(string medicineName,int demand)
         {
+            _log4net.Info(" Http GetSupplies request Initiated in MedicineSupplyRepository for " + medicineName + " with demand count " + demand);
             //List of Supply to be sent
             List<PharmacyMedicineSupply> supplies = new List<PharmacyMedicineSupply>();
             var stock = new List<MedicineStock>();
-
-            using (var httpclient = new HttpClient())
+            try
             {
-                httpclient.BaseAddress = new Uri("https://localhost:44366/");
-                HttpResponseMessage res = await httpclient.GetAsync("MedicineStockInformation");
-                if (res.IsSuccessStatusCode)
+                using (var httpclient = new HttpClient())
                 {
-                    var result = res.Content.ReadAsStringAsync().Result;
-                    stock = JsonConvert.DeserializeObject<List<MedicineStock>>(result);
+                    httpclient.BaseAddress = new Uri("https://localhost:44366/");
+                    HttpResponseMessage res = await httpclient.GetAsync("MedicineStockInformation");
+                    _log4net.Info(" Http GetSupplies request Initiated for the Medicine Api");
+                    if (res.IsSuccessStatusCode)
+                    {
+                        _log4net.Info(" Http GetSupplies data recieved from the Stock Api");
+                        var result = res.Content.ReadAsStringAsync().Result;
+                        stock = JsonConvert.DeserializeObject<List<MedicineStock>>(result);
+                    }
+                    else
+                    {
+                        _log4net.Error(" Http GetSupplies no data recieved from the Stock Api");
+                        return null;
+                    }
                 }
-            }
-
-            //Dictionary to store the Name of med and stock value as key value pair
-            Dictionary<string, int> meds = new Dictionary<string, int>();
-            foreach (var medicine in stock)
-            {
-                meds.Add(medicine.Name, medicine.NumberOfTabletsInStock);
-            }
-
-            //List of Pharmacy the company does business with
-            List<string> Pharmacies = new List<string>() { "Pharmacy1", "Pharmacy2", "Pharmacy3", "Pharmacy4" };
-            int totalPharmacies = Pharmacies.Count;
-            PharmacyMedicineSupply medSupply;
 
 
-            int inStock = meds[medicineName];
-            int demandStock = demand;
-            if (inStock > demandStock)
-            {
-                int medCount = demandStock / totalPharmacies;
-                for (int i = 0; i < totalPharmacies - 1; i++)
+
+                //Dictionary to store the Name of med and stock value as key value pair
+                Dictionary<string, int> meds = new Dictionary<string, int>();
+                foreach (var medicine in stock)
                 {
-                    medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[i], SupplyCount = medCount };
+                    meds.Add(medicine.Name, medicine.NumberOfTabletsInStock);
+                }
+
+                //List of Pharmacy the company does business with
+                List<string> Pharmacies = new List<string>() { "Max Pharmacy", "Appolo Pharmacy", "Synergy Pharmacy", "GoodWill Pharmacy" };
+                int totalPharmacies = Pharmacies.Count;
+                PharmacyMedicineSupply medSupply;
+
+                //Equal distribution of Medicine in Demand
+                int inStock = meds[medicineName];
+                int demandStock = demand;
+                if (inStock > demandStock)
+                {
+                    int medCount = demandStock / totalPharmacies;
+                    for (int i = 0; i < totalPharmacies - 1; i++)
+                    {
+                        medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[i], SupplyCount = medCount };
+                        supplies.Add(medSupply);
+                        demandStock = demandStock - medCount;
+                    }
+                    medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[totalPharmacies - 1], SupplyCount = demandStock };
                     supplies.Add(medSupply);
-                    demandStock = demandStock - medCount;
                 }
-                medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[totalPharmacies - 1], SupplyCount = demandStock };
-                supplies.Add(medSupply);
-            }
-            else
-            {
-                int medCount = inStock / totalPharmacies;
-                for (int i = 0; i < totalPharmacies - 1; i++)
+                else
                 {
-                    medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[i], SupplyCount = medCount };
+                    int medCount = inStock / totalPharmacies;
+                    for (int i = 0; i < totalPharmacies - 1; i++)
+                    {
+                        medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[i], SupplyCount = medCount };
+                        supplies.Add(medSupply);
+                        inStock = inStock - medCount;
+                    }
+                    medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[totalPharmacies - 1], SupplyCount = inStock };
                     supplies.Add(medSupply);
-                    inStock = inStock - medCount;
                 }
-                medSupply = new PharmacyMedicineSupply() { MedicineName = medicineName, PharmacyName = Pharmacies[totalPharmacies - 1], SupplyCount = inStock };
-                supplies.Add(medSupply);
-            }
 
+                return supplies;
+            }
+            catch(Exception ex)
+            {
+                _log4net.Error(" Http GetSupplies Could not Connect to the Medicine Stock Api"+ex.Message);
+            }
             return supplies;
         }
     }
